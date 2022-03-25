a050d57a699a73703292c677c3049189b81faa3f
BAM-hub <bsharamin12@gmail.com>
2022-03-25 18:33:56 +0300

    first commit

---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+import Layout from '@/components/Layout';
+
+export default function AddEvaentPage() {
+  return (
+    <Layout title='Add New Event'>
+      <div>AddEvaentPage</div>
+    </Layout>
+  )
+}
